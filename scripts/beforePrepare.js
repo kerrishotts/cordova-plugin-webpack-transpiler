2c3bd64359c4acce780c2eb1a474a342e41dfce4
Kerri Shotts <kerrishotts@gmail.com>
2017-02-15 16:59:23 -0600

    require webpack from project root to fix link
    also emit better errors & clean up code

--- scripts/beforePrepare.js
+++ scripts/beforePrepare.js
@@ -1,27 +1,57 @@
 #!/usr/bin/env node
 
-var webpack = require("webpack");
+
+/**
+ *
+ * Returns a prettified stringified representation of item
+ * @param {any} item    the item to pretty print
+ * @returns {string}    the pretty printed transformation
+ */
+function prettyPrint(item) {
+    return JSON.stringify(item, null, 2);
+}
 
 module.exports = function(ctx) {
-    var shell = ctx.requireCordovaModule("shelljs"),
-        path = ctx.requireCordovaModule("path"),
+
+    /* eslint-disable global-require */
+    // why? Because if we are linked to the plugin, node doesn't know how to find our
+    // modules! So we have to have the project root directory and then resolve where
+    // webpack lives. Fun, fun, fun!
+    var webpack = require(ctx.opts.projectRoot + "/node_modules/webpack/lib/webpack.js");
+
+    /* eslint-enable global-require */
+    var path = ctx.requireCordovaModule("path"),
         fs = ctx.requireCordovaModule("fs"),
         Q = ctx.requireCordovaModule("q"),
-        events = ctx.requireCordovaModule("cordova-common").events,
-        debugWebpackConfigPath = path.join(ctx.opts.projectRoot, "webpack.config.js");
-        releaseWebpackConfigPath = path.join(ctx.opts.projectRoot, "webpack.release.config.js");
-        debugWebpackConfig = require(debugWebpackConfigPath),
-        webpackConfig = undefined,
-        deferral = new Q.defer();
+        events = ctx.requireCordovaModule("cordova-common").events;
+
+    var debugWebpackConfigPath = path.join(ctx.opts.projectRoot, "webpack.config.js"),
+        releaseWebpackConfigPath = path.join(ctx.opts.projectRoot, "webpack.release.config.js"),
+        webpackConfig,
+        deferral = Q.defer();
+
+    /* eslint-disable global-require */
+    var debugWebpackConfig = require(debugWebpackConfigPath);
+    var releaseWebpackConfig;
+
+    /* eslint-enable global-require */
 
     events.emit("info", "Starting webpack bundling and typescript transpilation phase...");
-    if (ctx.cmdLine.indexOf("--release") > -1) {
+
+    if (ctx.cmdLine.toLowerCase().indexOf("--release") > -1) {
+        // release mode gets different settings
         events.emit("verbose", "... building release bundle");
         if (fs.existsSync(releaseWebpackConfigPath)) {
+            // use user-provided webpack settings if they exist
+
+            /* eslint-disable global-require */
+            releaseWebpackConfig = require(releaseWebpackConfigPath);
+
+            /* eslint-enable global-require */
             events.emit("verbose", "... ... with existing release webpack config");
-            var releaseWebpackConfig = require(releaseWebpackConfigPath);
-            webpackConfig = Object.assign({}, debugWebpackConfig, releaseWebpacConfig);
+            webpackConfig = Object.assign({}, debugWebpackConfig, releaseWebpackConfig);
         } else {
+            // otherwise use some basic defaults (uglify & no source maps)
             events.emit("verbose", "... ... with internal release webpack config");
             webpackConfig = Object.assign({}, debugWebpackConfig, {
                 devtool: "none",
@@ -33,31 +63,34 @@ module.exports = function(ctx) {
             });
         }
     } else {
+        // use the debug configuration
         events.emit("verbose", "... building debug bundle");
         webpackConfig = debugWebpackConfig;
     }
 
     webpack(webpackConfig, function(err, stats) {
+        var prettyStats = stats.toString({chunks: false, colors: false});
         if (err || stats.hasErrors() || stats.hasWarnings()) {
             if (err) {
                 events.emit("error", "Webpack generated an error!");
-                events.emit("error", JSON.stringify(err.stack || err));
+                events.emit("error", prettyPrint(err.stack || err));
                 if (err.details) {
-                    events.emit("error", JSON.stringify(err.details));
+                    events.emit("error", prettyPrint(err.details));
                 }
                 deferral.reject(err);
             }
             if (stats.hasErrors()) {
                 events.emit("error", "Webpack generated an error!");
-                events.emit("error", JSON.stringify(stats.toJson().errors));
-                deferral.reject(stats.toJson());
+                events.emit("error", prettyStats);
+                deferral.reject(stats);
             }
             if (stats.hasWarnings()) {
                 events.emit("warn", "Webpack generated warnings!");
-                events.emit("warn", JSON.stringify(stats.toJson().warnings));
+                events.emit("warn", prettyStats);
             }
         }
         events.emit("info", "... webpack bundling and typescript transpilation phase complete!");
+        events.emit("info", prettyStats);
         deferral.resolve();
     });
 
