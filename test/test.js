bbe18d66224265e4934d891672e4f74867cfaf82
Kerri Shotts <kerrishotts@gmail.com>
2017-02-17 16:37:06 -0600

    fix test?

--- test/test.js
+++ test/test.js
@@ -9,29 +9,30 @@ describe("Create project using default configuration", function() {
     var pluginDir = process.cwd();
     var tmp = tempdir();
 
-    it("Should be able to create a simple Cordova project", function() {
+    it("Should be able to create and transpile a simple Cordova project", function() {
         cd(tmp);
-        exec("cordova create test com.example.test test");
+        exec("cordova create test com.example.test test --verbose");
         cd("test");
-        exec("cordova platform add --save ios");
+        exec("cordova platform add ios --verbose");
         expect(ls("*.*").length).to.be.greaterThan(0);
-    });
 
-    it ("Should be able to add this plugin", function() {
-        exec("cordova plugin add --save " + pluginDir);
-        expect(ls("plugins/cordova-plugin-webpack-transpiler/*.*").length).to.be.greaterThan(0);
-    });
+        it ("Should be able to add this plugin", function() {
+            exec("cordova plugin add --verbose --save " + pluginDir);
+            expect(ls("plugins/cordova-plugin-webpack-transpiler/*.*").length).to.be.greaterThan(0);
+        });
 
-    it ("Should be able to transpile by default", function() {
-        // copy assets first
-        cp("-rf", path.join(pluginDir, "example-ts", "www", "esm"), path.join(tmp, "test", "www"));
+        it ("Should be able to transpile by default", function() {
+            // copy assets first
+            cp("-rf", path.join(pluginDir, "example-ts", "www", "esm"), path.join(tmp, "test", "www"));
 
-        exec("cordova prepare --verbose");
+            exec("cordova prepare --verbose");
 
-        // check outputs
-        expect(ls("www/js/app.bundle.js").length).to.be.equal(1);
-        expect(ls("platforms/ios/www/js/app.bundle.js").length).to.be.equal(1);
-        expect(ls("platforms/ios/www/esm").length).to.be.equal(0);
+            // check outputs
+            expect(ls("www/js/app.bundle.js").length).to.be.equal(1);
+            expect(ls("platforms/ios/www/js/app.bundle.js").length).to.be.equal(1);
+            expect(ls("platforms/ios/www/esm").length).to.be.equal(0);
+        });
+
     });
 
 });
