65055d5443926b4d7386224abd4360690d573c73
Kerri Shotts <kerrishotts@gmail.com>
2017-02-21 23:02:19 -0600

    new logic for webpack

--- assets/babel/webpack.config.js
+++ assets/babel/webpack.config.js
@@ -1,29 +1,122 @@
-var path = require("path"),
-    fs = require("fs");
+/*******************************************************************************
+ *
+ * cordova-plugin-webpack-transpiler webpack.config.js
+ * ---------------------------------------------------
+ *
+ * Feel free to modify this file to your needs; it will not be overwritten by
+ * the plugin unless you delete the file entirely.
+ *
+ * Notes:
+ *
+ *   - External/Sibling mode is detected automatically. When in External mode,
+ *     additional assets can be copied (for example, from "src -> www").
+ *   - The names of all the files and paths are not hard-coded; you can change
+ *     them below to match your configuration.
+ *
+ ******************************************************************************/
 
-var wwwFolder = path.resolve(__dirname, "www"),
-    siblingESFolder = path.resolve(wwwFolder, "esm"),
-    externalESFolder = path.resolve(__dirname, "esm"),
-    entryPath = fs.existsSync(externalESFolder) ? externalESFolder : siblingESFolder,
-    outputPath = path.resolve(wwwFolder, "js"),
-    jsBundleName = "app.bundle.js",
-    entryFile = path.resolve(entryPath, "index.js");
+var path = require("path"),
+    fs = require("fs"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin");
+
+var src = path.resolve(__dirname, "src"),
+    assetsToCopy = [];
+
+/*
+ * These files and globs will be copied if using the external configuration
+ * (src -> www).
+ *
+ * For full information, see https://github.com/kevlened/copy-webpack-plugin
+ ******************************************************************************/
+var assetsToCopyIfExternal = [
+    { from: "index.html" },
+    { from: "img/**/*" },
+    { from: "html/**/*" },
+];
+
+if (!fs.existsSync(src)) {
+    src = path.resolve(__dirname, "www");
+} else {
+    assetsToCopy = assetsToCopyIfExternal;
+}
+
+/*
+ * it is assumed that you will be using "es/*.js" for ES2015+ and "scss/*.scss"
+ * for SASS. You can change these as needed.
+ ******************************************************************************/
+var sourcePaths = {
+    src: src,
+    es: "es",
+    scss: "scss"
+};
+
+/*
+ * It is assumed that your output is going to "www", and that the JavaScript
+ * should be stored in "www/js", and that any generated CSS should be stored in
+ * "www/css".
+ ******************************************************************************/
+var outputPaths = {
+    www: "www",
+    js: "js",
+    css: "css"
+}
+
+/*
+ * Feel free to change the following if these assumptions are incorrect:
+ *   - If you are using SASS, the entry file will be named "styles.scss" and
+ *     will be copied to bundle.css
+ *   - If you are using ES2015+, the entry file will be named "index.js" and
+ *     will be copied to bundle.js
+ ******************************************************************************/
+var indexes = {
+    scss: { from: path.join(sourcePaths.scss, "styles.scss"), to: path.join(outputPaths.css, "bundle.css") },
+      es: { from: path.join(sourcePaths.es, "index.js"), to: path.join(outputPaths.js, "bundle.js") },
+}
+
+/*
+ * If you're just changing file and path names, you don't need to proceed any
+ * further. If you need to change the logic, proceed.
+ ******************************************************************************/
+
+var jsEntryFile = indexes.es.from,
+    sassEntryFile = indexes.scss.from,
+    outputFile = indexes.es.to,
+    extractSass = new ExtractTextPlugin(indexes.scss.to);
 
 module.exports = {
-    context: entryPath,
-    entry: entryFile,
+    context: sourcePaths.src,
+    entry: ["./" + jsEntryFile, "./" + sassEntryFile],
     devtool: "inline-source-map",
     output: {
-        filename: jsBundleName,
-        path: outputPath
+        filename: outputFile,
+        path: outputPaths.www,
     },
     module: {
-        loaders: [
+        rules: [
+            { test: /\.(html|txt)$/, use: "raw-loader" },
+            { test: /\.(png|jpg|svg)$/, use: ["file-loader"]},
+            { test: /\.(json|json5)$/, use: "json5-loader" },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    fallback: "style-loader",
+                    use: [
+                             { loader: "css-loader?sourceMap=true&import=false&url=false"},
+                             { loader: "resolve-url-loader?sourceMap=true"},
+                             { loader: "sass-loader?sourceMap=true"}
+                            ]
+                })
+            },
             {
                 test: /\.js$/,
-                loader: "babel-loader",
+                use: [ "babel-loader"],
                 exclude: /node_modules/
-            }
+            },
         ]
-    }
+    },
+    plugins: [
+        extractSass,
+        new CopyWebpackPlugin(assetsToCopy)
+    ]
 }
--- assets/typescript/webpack.config.js
+++ assets/typescript/webpack.config.js
@@ -1,34 +1,128 @@
-var path = require("path"),
-    fs = require("fs");
+/*******************************************************************************
+ *
+ * cordova-plugin-webpack-transpiler webpack.config.js
+ * ---------------------------------------------------
+ *
+ * Feel free to modify this file to your needs; it will not be overwritten by
+ * the plugin unless you delete the file entirely.
+ *
+ * Notes:
+ *
+ *   - External/Sibling mode is detected automatically. When in External mode,
+ *     additional assets can be copied (for example, from "src -> www").
+ *   - The names of all the files and paths are not hard-coded; you can change
+ *     them below to match your configuration.
+ *
+ ******************************************************************************/
 
-var wwwFolder = path.resolve(__dirname, "www"),
-    siblingESFolder = path.resolve(wwwFolder, "esm"),
-    externalESFolder = path.resolve(__dirname, "esm"),
-    entryPath = fs.existsSync(externalESFolder) ? externalESFolder : siblingESFolder,
-    tsEntry = path.resolve(entryPath, "index.ts"),
-    jsEntry = path.resolve(entryPath, "index.js"),
-    outputPath = path.resolve(wwwFolder, "js"),
-    jsBundleName = "app.bundle.js",
-    entryFile = fs.existsSync(tsEntry) ? tsEntry : jsEntry;
+var path = require("path"),
+    fs = require("fs"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin");
+
+var src = path.resolve(__dirname, "src"),
+    assetsToCopy = [];
+
+/*
+ * These files and globs will be copied if using the external configuration
+ * (src -> www).
+ *
+ * For full information, see https://github.com/kevlened/copy-webpack-plugin
+ ******************************************************************************/
+var assetsToCopyIfExternal = [
+    { from: "index.html" },
+    { from: "img/**/*" },
+    { from: "html/**/*" },
+];
+
+if (!fs.existsSync(src)) {
+    src = path.resolve(__dirname, "www");
+} else {
+    assetsToCopy = assetsToCopyIfExternal;
+}
+
+/*
+ * it is assumed that you will be using "es/*.js" for ES2015+, "ts/*.ts" for
+ * TypeScript, and "scss/*.scss" for SASS. You can change these as needed.
+ ******************************************************************************/
+var sourcePaths = {
+    src: src,
+    es: "es",
+    ts: "ts",
+    scss: "scss"
+};
+
+/*
+ * It is assumed that your output is going to "www", and that the JavaScript
+ * (or TypeScript) should be stored in "www/js", and that any generated CSS
+ * should be stored in "www/css".
+ ******************************************************************************/
+var outputPaths = {
+    www: "www",
+    js: "js",
+    css: "css"
+}
+
+/*
+ * Feel free to change the following if these assumptions are incorrect:
+ *   - If you are using SASS, the entry file will be named "styles.scss" and
+ *     will be copied to bundle.css
+ *   - If you are using ES2015+, the entry file will be named "index.js" and
+ *     will be copied to bundle.js
+ *   - If you are using TypeScript, the entry file will be named "index.ts"
+ *     and will be copied to bundle.js
+ ******************************************************************************/
+var indexes = {
+    scss: { from: path.join(sourcePaths.scss, "styles.scss"), to: path.join(outputPaths.css, "bundle.css") },
+      es: { from: path.join(sourcePaths.es, "index.js"), to: path.join(outputPaths.js, "bundle.js") },
+      ts: { from: path.join(sourcePaths.ts, "index.ts"), to: path.join(outputPaths.js, "bundle.js") },
+}
+
+/*
+ * If you're just changing file and path names, you don't need to proceed any
+ * further. If you need to change the logic, proceed.
+ ******************************************************************************/
+
+var usingTypeScript = fs.existsSync(path.resolve(sourcePaths.src, sourcePaths.ts));
+
+var jsEntryFile = usingTypeScript ? indexes.ts.from : indexes.es.from,
+    sassEntryFile = indexes.scss.from,
+    outputFile = usingTypeScript ? indexes.ts.to : indexes.es.to,
+    extractSass = new ExtractTextPlugin(indexes.scss.to);
 
 module.exports = {
-    context: entryPath,
-    entry: entryFile,
+    context: sourcePaths.src,
+    entry: ["./" + jsEntryFile, "./" + sassEntryFile],
     devtool: "inline-source-map",
     output: {
-        filename: jsBundleName,
-        path: outputPath
+        filename: outputFile,
+        path: outputPaths.www,
     },
     module: {
-        loaders: [
+        rules: [
+            { test: /\.(html|txt)$/, use: "raw-loader" },
+            { test: /\.(png|jpg|svg)$/, use: ["file-loader"]},
+            { test: /\.(json|json5)$/, use: "json5-loader" },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    fallback: "style-loader",
+                    use: [
+                             { loader: "css-loader?sourceMap=true&import=false&url=false"},
+                             { loader: "resolve-url-loader?sourceMap=true"},
+                             { loader: "sass-loader?sourceMap=true"}
+                            ]
+                })
+            },
             {
                 test: /\.(ts|js)$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
-                options: {
-                    entryFileIsJs: entryFile === jsEntry
-                }
-            }
+                use: [ "ts-loader" + (usingTypeScript ? "" : "?entryFileIsJs") ],
+                exclude: /node_modules/
+            },
         ]
-    }
+    },
+    plugins: [
+        extractSass,
+        new CopyWebpackPlugin(assetsToCopy)
+    ]
 }
