93e552c03c825fad3dd2504c95249fea24bb0ea4
Kerri Shotts <kerrishotts@gmail.com>
2017-03-17 15:11:10 -0500

    Lots! improve aliases bundle css if not using scss fix defaults additional options

--- config/_common/webpack.common.js
+++ config/_common/webpack.common.js
@@ -43,14 +43,16 @@ var dirs = {
     vendor: "vendor",
     www: "www",
     aliases: {
-        Components: "components",
-        Controllers: "controllers",
-        Models: "models",
-        Pages: "pages",
-        Routes: "routes",
-        Templates: "templates",
-        Utilities: "util",
-        Views: "views",
+        Lib: "lib",
+        Vendor: "vendor",
+        Components: "$JS/components",
+        Controllers: "$JS/controllers",
+        Models: "$JS/models",
+        Pages: "$JS/pages",
+        Routes: "$JS/routes",
+        Templates: "$JS/templates",
+        Utilities: "$JS/util",
+        Views: "$JS/views",
     }
 }
 
@@ -82,23 +84,34 @@ var vendor = [];
 
 function config(options) {
     var src = options.src,
-        assetsToCopyIfExternal = options.assetsToCopyIfExternal,
-        assetsToCopyIfSibling = options.assetsToCopyIfSibling,
-        sourcePaths = options.sourcePaths,
-        entryFiles = options.entryFiles,
-        outputFile = options.outputFile,
-        outputPaths = options.outputPaths,
-        indexes = options.indexes,
-        dirs = options.dirs,
-        extensions = options.extensions,
-        vendor = options.vendor,
         allowTypeScript = options.allowTypeScript || false,
         allowScss = options.allowScss || false,
-        transpiler = options.transpiler || (allowTypeScript ? "ts-loader" : "babel-loader"),
+        assetsToCopy = [],
+        assetsToCopyIfExternal = options.assetsToCopyIfExternal,
+        assetsToCopyIfSibling = options.assetsToCopyIfSibling,
+        cssFallbackLoader = options.cssFallbackLoader || "style-loader",
+        dirs = options.dirs,
+        devtool = options.devtool || "inline-source-map",
+        entryFiles = options.entryFiles,
+        extensions = options.extensions || [
+                ".js", ".ts", ".jsx", ".es", // typical JS extensions
+                ".jsm", ".esm",               // jsm is node's ES6 module ext
+                ".json",                      // some modules require json without an extension
+                ".css", ".scss",              // CSS & SASS extensions
+                "*"
+                ],                         // allow extensions on imports
+        indexes = options.indexes,
         mode = options.mode || process.env.NODE_ENV || "development",
-        operatingMode = "external";
-    var assetsToCopy = [];
+        operatingMode = "external",
+        outputFile = options.outputFile,
+        outputPaths = options.outputPaths,
+        sourcePaths = options.sourcePaths,
+        transpiler = options.transpiler || (allowTypeScript ? "ts-loader" : "babel-loader"),
+        vendor = options.vendor;
 
+    /*
+     * determine operating mode (external vs sibling)
+     *****************************************************************************/
     if (!src) {
         src = path.resolve(__dirname, dirs.external);
         assetsToCopy = assetsToCopyIfExternal;
@@ -114,23 +127,24 @@ function config(options) {
     * it is assumed that you will be using "es/*.js" for ES2015+, "ts/*.ts" for
     * TypeScript, and "scss/*.scss" for SASS. You can change these as needed.
     ******************************************************************************/
-    sourcePaths = Object.assign({}, sourcePaths ? sourcePaths : {}, {
+    sourcePaths = Object.assign({}, {
         src: src,
         es: dirs.es,
         ts: dirs.ts,
+        css: dirs.css,
         scss: dirs.scss
-    });
+    }, sourcePaths ? sourcePaths : {});
 
     /*
     * It is assumed that your output is going to "www", and that the JavaScript
     * (or TypeScript) should be stored in "www/js", and that any generated CSS
     * should be stored in "www/css".
     ******************************************************************************/
-    outputPaths = Object.assign({}, outputPaths ? outputPaths : {}, {
+    outputPaths = Object.assign({}, {
         www: dirs.www,
         js: dirs.js,
         css: dirs.css
-    });
+    }, outputPaths ? outputPaths : {});
 
     /*
     * Feel free to change the following if these assumptions are incorrect:
@@ -141,32 +155,43 @@ function config(options) {
     *   - If you are using TypeScript, the entry file will be named "index.ts"
     *     and will be copied to bundle.js
     ******************************************************************************/
-    indexes = Object.assign({}, indexes ? indexes : "", {
+    indexes = Object.assign({}, {
+        css: { from: path.join(sourcePaths.css, "styles.css"), to: path.join(outputPaths.css, "bundle.css") },
         scss: { from: path.join(sourcePaths.scss, "styles.scss"), to: path.join(outputPaths.css, "bundle.css") },
         es: { from: path.join(sourcePaths.es, "index.js"), to: path.join(outputPaths.js, "bundle.js") },
         ts: { from: path.join(sourcePaths.ts, "index.ts"), to: path.join(outputPaths.js, "bundle.js") },
         vendor: { js: path.join(outputPaths.js, "vendor.js"), css: path.join(outputPaths.css, "vendor.css") }
-    });
+    }, indexes ? indexes : {});
 
+    // if allowTypeScript is true AND the typescript path in sourcePaths exists,
+    // we'll assume we're using typescript
     var usingTypeScript = allowTypeScript && fs.existsSync(path.resolve(sourcePaths.src, sourcePaths.ts));
 
+    // set up our entry files
     var jsEntryFile = usingTypeScript ? indexes.ts.from : indexes.es.from,
-        sassEntryFile = indexes.scss.from,
-        extractSass = allowScss ? new ExtractTextPlugin(indexes.scss.to) : null,
-        extractVendorCss = new ExtractTextPlugin("css/vendor.css");
+        cssEntryFile = allowScss ? indexes.scss.from : indexes.css.from;
 
+    // define our extract plugins for CSS; if allowScss is true, we assume using scss/styles.scss
+    var extractCss = new ExtractTextPlugin(allowScss ? indexes.scss.to : indexes.css.to),
+        extractVendorCss = new ExtractTextPlugin(indexes.vendor.css);
+
+    // If no output file specified, default to indexes.(es|ts).to
     if (!outputFile) {
         outputFile = usingTypeScript ? indexes.ts.to : indexes.es.to;
     }
 
+    // if no entry files passed in, we need to come up with suitable defaults
     if (!entryFiles) {
+        // include the javascript file
         entryFiles = { app: ["./" + jsEntryFile] };
 
-        if (allowScss && fs.existsSync(path.resolve(sourcePaths.src, sassEntryFile))) {
-            entryFiles.app.push("./" + sassEntryFile);
+        // and the (s)css file, if present
+        if (fs.existsSync(path.resolve(sourcePaths.src, cssEntryFile))) {
+            entryFiles.app.push("./" + cssEntryFile);
         }
     }
 
+    // if vendors have been provided, add them to entryFiles
     if (vendor.length > 0) {
         entryFiles.vendor = vendor;
     }
@@ -174,18 +199,13 @@ function config(options) {
     return {
         context: sourcePaths.src,
         entry: entryFiles,
-        devtool: "inline-source-map",
+        devtool: devtool,
         output: {
             filename: outputFile,
             path: outputPaths.www,
         },
         resolve: {
-            extensions: extensions ||
-            [".js", ".ts", ".jsx", ".es", // typical JS extensions
-                ".jsm", ".esm",               // jsm is node's ES6 module ext
-                ".json",                      // some modules require json without an extension
-                ".css", ".scss",              // CSS & SASS extensions
-                "*"],                         // allow extensions on imports
+            extensions: extensions,
             modules: [
                 path.resolve(sourcePaths.src, dirs.es, "lib"),
                 path.resolve(sourcePaths.src, dirs.es, "vendor"),
@@ -195,18 +215,17 @@ function config(options) {
                 path.resolve(sourcePaths.src, "vendor"),
                 "node_modules"
             ],
-            alias: Object.assign({}, {
-                "$LIB": path.join(dirs.lib),
-                "Lib": path.join(dirs.lib),
-                "$VENDOR": path.join(dirs.vendor),
-                "Vendor": path.join(dirs.vendor),
-            }, (function getAliases() {
-                var k, v, obj = {};
+            alias: Object.assign({}, (function getAliases() {
+                var k, v, obj = {}, p;
                 for (k in dirs.aliases) {
                     if (Object.prototype.hasOwnProperty.call(dirs.aliases, k)) {
                         v = dirs.aliases[k];
-                        obj["$" + k.toUpperCase()] = path.join(usingTypeScript ? dirs.ts : dirs.es, v);
-                        obj[k] = path.join(usingTypeScript ? dirs.ts : dirs.es, v);
+                        p = v;
+                        if (v.substr(0,4) === "$JS/") {
+                            p = path.join(usingTypeScript ? dirs.ts : dirs.es, v.substr(4));
+                        }
+                        obj["$" + k.toUpperCase()] = p;
+                        obj[k] = p;
                     }
                 }
                 return obj;
@@ -214,42 +233,49 @@ function config(options) {
         },
         module: {
             rules: [
+                // HTML & TEXT files should just be loaded as raw (included in bundle)
                 { test: /\.(html|txt)$/, use: "raw-loader" },
+                // Images and web fonts should be copied & file path returned
+                // note: copying only occurs when in external mode
                 {
                     test: /\.(png|jpe?g|svg|gif|eot|ttf|woff|woff2)$/,
                     use: ["file-loader?name=[path][name].[ext]&emitFile=" +
                       (operatingMode === "external" ? "true" : "false")]
                 },
+                // JSON/JSON5 should use the JSON5 loader and be included in bundle
                 { test: /\.(json|json5)$/, use: "json5-loader" },
+                // Extract our app's CSS into the bundle
                 {
-                    test: extractSass ? /\.s?css$/ : /\.css$/,
+                    test: extractCss ? /\.s?css$/ : /\.css$/,
                     exclude: /node_modules\/.*\.css$/,
-                    use: extractSass.extract({
-                        fallback: "style-loader",
+                    use: extractCss.extract({
+                        fallback: cssFallbackLoader,
                         use: ((function () {
                             var arr = [
                                 { loader: "css-loader?sourceMap=true" },
                                 { loader: "resolve-url-loader?sourceMap=true" },
                             ];
-                            if (extractSass) {
+                            if (cssEntryFile === indexes.scss.from && allowScss) {
                                 arr.push({ loader: "sass-loader?sourceMap=true" });
                             }
                             return arr;
                         })())
                     })
                 },
+                // css files in node_modules are put into a vendor bundle
                 {
                     test: /node_modules\/.*\.css$/,
                     use: extractVendorCss.extract({
-                        fallback: "style-loader",
+                        fallback: cssFallbackLoader,
                         use: [
                             { loader: "css-loader?sourceMap=true" },
                             { loader: "resolve-url-loader?sourceMap=true" },
                         ]
                     })
                 },
+                // JavaScript / TypeScript code
                 {
-                    test: /\.(js|jsx|ts|tsx)$/,
+                    test: allowTypeScript ? /\.(js|jsx|ts|tsx)$/ : /\.(js|jsx)$/,
                     use: [transpiler + (allowTypeScript ? (usingTypeScript ? "" : "?entryFileIsJs") : "")],
                     exclude: /node_modules/
                 },
@@ -258,8 +284,11 @@ function config(options) {
         plugins: (function () {
             var plugins = [
                 new CopyWebpackPlugin(assetsToCopy),
+                extractCss
             ];
 
+            // only generate index.html with dependencies IF we're in external mode --
+            // otherwise we'd overwrite our user's own index.html in www
             if (operatingMode === "external") {
                 plugins.push(new HtmlWebpackPlugin({
                     filename: "index.html",
@@ -269,15 +298,14 @@ function config(options) {
                 }));
             }
 
-            if (extractSass) {
-                plugins.push(extractSass);
-            }
+            // only create the JS vendor chunk if we've been given vendors
             if (vendor.length > 0) {
                 plugins.push(new webpack.optimize.CommonsChunkPlugin({
                     name: "vendor",
                     filename: indexes.vendor.js
                 }));
             }
+
             return plugins;
         }())
     };
